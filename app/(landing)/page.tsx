a21a3eacd9379e82e4e2e033c1513a437f3be669
shaulov <shineginn@gmail.com>
2024-01-12 16:50:00 +0300

    Add sign-in and sign-up pages

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default function LandingPage() {
 	return (
-		<p>
-			Landing Page
-		</p>
+		<>
+			<p>
+				Landing Page
+			</p>
+			<Button asChild>
+				<Link href="/sign-in">Login</Link>
+			</Button>
+			<Button asChild>
+				<Link href="/sign-up">Register</Link>
+			</Button>
+		</>
 	);
 }
