4442db0156e47cc69543c2760fe50e8d3e386cb1
shaulov <shineginn@gmail.com>
2024-01-17 17:57:38 +0300

    Add layout for main page

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,20 @@
+import Navbar from '@/components/navbar';
+import Sidebar from '@/components/sidebar';
+
+export default function DashboardLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<div className="relative h-full">
+			<div className="fixed inset-y-0 z-[1] hidden md:flex md:flex-col h-full w-72 bg-gray-900">
+				<Sidebar />
+			</div>
+			<main className="md:pl-72">
+				<Navbar />
+				{children}
+			</main>
+		</div>
+	);
+}
