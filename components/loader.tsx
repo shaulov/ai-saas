cd200c32f160705605ff401aa3012cca2e812bc6
shaulov <shineginn@gmail.com>
2024-01-22 16:18:18 +0300

    Add loader component

---
+++ components/loader.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+function Loader() {
+  return (
+    <div className="grid items-center justify-center gap-y-4 h-full">
+      <div className="justify-self-center relative w-10 h-10 animate-spin">
+        <Image src="/logo.png" fill alt="Loader image" />
+      </div>
+      <p className="text-muted-foreground text-sm text-center">Genius is thinking...</p>
+    </div>
+  );
+}
+
+export default Loader;
