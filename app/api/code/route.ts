37c460aca1e4de70c797353b461a42791b49fcef
shaulov <shineginn@gmail.com>
2024-01-23 15:28:18 +0300

    Change instruction message for code api

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -11,7 +11,7 @@ const client = new GigaChat(
 );
 const instructionMessage = {
   role: "system",
-  content: "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.",
+  content: "Ты генератор кода. Ты должен отвечать только с помощью снипетов кода. Используй комментарии кода для объяснения.",
 }
 
 export async function POST(req: Request) {
