153caf4911a2a87c1752f1a5def20191f03e4f3f
shaulov <shineginn@gmail.com>
2024-01-23 15:24:48 +0300

    Add code page

---
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Code } from "lucide-react";
+import { useForm } from "react-hook-form";
+import ReactMarkdown from "react-markdown";
+import { cn } from "@/lib/utils";
+import Heading from "@/components/heading";
+import Empty from "@/components/empty";
+import Loader from "@/components/loader";
+import BotAvatar from "@/components/bot-avatar";
+import UserAvatar from "@/components/user-avatar";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { formSchema } from "./constants";
+
+export default function CodePage() {
+  const router = useRouter();
+  const [messages, setMessages] = useState<any[]>([]);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+  const handleSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const userMessage: any = {
+        role: "user",
+        content: values.prompt,
+      };
+      const newMessages = [...messages, userMessage];
+
+      const response = await axios.post("/api/code", {
+        messages: newMessages,
+      });
+
+      setMessages((current) => [...current, userMessage, response.data]);
+      form.reset();
+    } catch (error) {
+      // TODO: Open pro modal
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <section>
+      <Heading
+        title="Code Generation"
+        description="Generate code using descriptive text"
+        icon={Code}
+        iconColor="text-green-700"
+        bgColor="bg-green-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <Form {...form}>
+          <form
+            className="grid grid-cols-12 gap-2 w-full p-4 px-3 md:px-6 border rounded-lg focus-within:shadow-sm"
+            onSubmit={form.handleSubmit(handleSubmit)}
+          >
+            <FormField
+              name="prompt"
+              render={({ field }) => (
+                <FormItem className="col-span-12 lg:col-span-10">
+                  <FormControl className="m-0 p-0">
+                    <Input
+                      className="border-0 rounded-none outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:border-b focus-visible:border-violet-500"
+                      disabled={isLoading}
+                      placeholder="Simple toggle button using react hooks."
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button
+              className="col-span-12 lg:col-span-2 w-full"
+              disabled={isLoading}
+            >
+              Generate
+            </Button>
+          </form>
+        </Form>
+        <div className="mt-4 space-y-4">
+          {isLoading && (
+            <div className="flex items-center justify-center w-full p-8 bg-muted rounded-lg">
+              <Loader />
+            </div>
+          )}
+          {messages.length === 0 && !isLoading && (
+            <Empty label="No conversation started" />
+          )}
+          <ul className="flex flex-col-reverse gap-y-4">
+            {messages.map((message) => (
+              <li
+                className={cn(
+                  "flex items-start gap-x-4 w-full p-8 rounded-lg",
+                  message.role === "user"
+                    ? "bg-white border-black/10"
+                    : "bg-muted",
+                )}
+                key={message.content}
+              >
+                {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+                <ReactMarkdown 
+                  className="overflow-hidden text-sm leading-7"
+                  components={{
+                    pre: ({ node, ...props }) => (
+                      <div className="w-full my-2 p-2 bg-black/10 rounded-lg overflow-auto">
+                        <pre {...props} />
+                      </div>
+                    ),
+                    code: ({ node, ...props }) => (
+                      <code className="p-1 bg-black/10 rounded-lg" {...props} />
+                    ),
+                  }}
+                >
+                  {message.content || ""}
+                </ReactMarkdown>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
