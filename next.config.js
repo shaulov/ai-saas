f397b4ff97c06a3cf8dfba7d4f0a382c5c887590
shaulov <shineginn@gmail.com>
2024-01-26 15:17:07 +0300

    Add image path to config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      "replicate.delivery"
+    ],
+  },
+}
 
 module.exports = nextConfig
