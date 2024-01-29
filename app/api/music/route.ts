667b13db394a1b4da57f07f57485538643993027
shaulov <shineginn@gmail.com>
2024-01-29 16:28:08 +0300

    Add music API route

---
+++ app/api/music/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+import Replicate from "replicate";
+
+const apiKey = process.env.REPLICATE_API_TOKEN || '';
+const replicate = new Replicate({
+  auth: apiKey,
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthoriazed", { status: 401 });
+    }
+
+    if (!apiKey) {
+      return new NextResponse("API Key not configured", { status: 500 });
+    }
+
+    if (!prompt) {
+      return new NextResponse("Music prompt are required", { status: 400 });
+    }
+
+    const output = await replicate.run(
+      "meta/musicgen:b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38",
+      {
+        input: {
+          model_version: "stereo-melody-large",
+          prompt,
+        }
+      }
+    );
+    
+    return NextResponse.json(output);
+  } catch (error) {
+    console.log("[MUSIC_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
