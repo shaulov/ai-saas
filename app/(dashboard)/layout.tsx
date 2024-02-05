d993c62969754e08e55c14d18fd32c92ed8d9799
shaulov <shineginn@gmail.com>
2024-02-05 15:20:21 +0300

    Add free counter component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,15 +1,18 @@
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
+import { getApiLimitCount } from "@/lib/api-limit";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const apiLimitCount = await getApiLimitCount();
+
   return (
     <div className="relative h-full">
       <div className="fixed inset-y-0 z-[1] hidden md:flex md:flex-col h-full w-72 bg-gray-900">
-        <Sidebar />
+        <Sidebar apiLimitCount={apiLimitCount} />
       </div>
       <main className="md:pl-72">
         <Navbar />
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -14,6 +14,7 @@ import {
   VideoIcon,
 } from "lucide-react";
 import { cn } from "@/lib/utils";
+import FreeCounter from "@/components/free-counter";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
@@ -61,7 +62,11 @@ const routes = [
   },
 ];
 
-function Sidebar() {
+interface SidebarProps {
+  apiLimitCount: number;
+}
+
+function Sidebar({ apiLimitCount }: SidebarProps) {
   const pathname = usePathname();
 
   return (
@@ -103,6 +108,7 @@ function Sidebar() {
           ))}
         </ul>
       </div>
+      <FreeCounter apiLimitCount={apiLimitCount} />
     </nav>
   );
 }
