d1e27c16add16ed244da548d8d66d8aa6f09227c
shaulov <shineginn@gmail.com>
2024-02-07 15:06:41 +0300

    Add stripe and its webhook API routes and add payment action to pro-modal

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+import axios from "axios";
 import { Check, Zap } from "lucide-react";
 import { useProModal } from "@/hooks/use-pro-modal";
 import { cn } from "@/lib/utils";
@@ -18,6 +20,20 @@ import { tools } from "@/const";
 
 function ProModal() {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const handleSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log(error, "STRIPE_CLIENT_ERROR");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -26,12 +42,17 @@ function ProModal() {
           <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
             <span className="flex items-center gap-x-2 py-1 font-bold">
               Upgrade to Genius
-              <Badge className="py-1 uppercase text-sm" variant="premium">pro</Badge>
+              <Badge className="py-1 uppercase text-sm" variant="premium">
+                pro
+              </Badge>
             </span>
           </DialogTitle>
-          <DialogDescription className="pt-2 text-center font-medium text-zinc-900" asChild>
+          <DialogDescription
+            className="pt-2 text-center font-medium text-zinc-900"
+            asChild
+          >
             <ul className="space-y-2">
-              {tools.map(tool => (
+              {tools.map((tool) => (
                 <li key={tool.label}>
                   <Card className="flex items-center gap-x-4 p-3 border-black/5">
                     <span className={cn("w-fit p-2 rounded-md", tool.bgColor)}>
@@ -46,7 +67,13 @@ function ProModal() {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button className="w-full" size="lg" variant="premium">
+          <Button
+            className="w-full"
+            size="lg"
+            variant="premium"
+            onClick={handleSubscribe}
+            disabled={loading}
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
